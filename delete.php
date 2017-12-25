dbeb1ca46a4ee2402ddb5ddcc4de07c931ec5f78
Shawon <rahman188175@gmail.com>
2017-12-25 17:31:03 +0600

    A dynamic website

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php 
+require 'db.php';
+$id = $_GET['id'];
+$sql = "DELETE FROM people WHERE id=$id";
+if ($connection->query($sql)) {
+  header("Location: /show.php");
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require 'header.php'; ?>
+
+<div class="container mt-5">
+    <ul class="list-group">
+        <li class="list-group-item"><a href="create.php">Create</a></li>
+        <li class="list-group-item"><a href="show.php">Show</a></li>
+    </ul>
+</div>
+
+<?php require 'footer.php';?>
